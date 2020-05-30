a04d037503bdd6e1f13d76f8128a83915dcd4723
crabdul <23074754+crabdul@users.noreply.github.com>
2020-05-30 10:53:22 +0100

    WIP: Need query renderer somewhere
    Hints:
    Tried looking at
    https://relay-modern-course.now.sh/packages/03b-create-query-renderer/

--- workshop/01-fetchGraphql/src/App.tsx
+++ workshop/01-fetchGraphql/src/App.tsx
@@ -1,59 +1,10 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import { Flex, Text } from 'rebass'
-import { Environment, Network, RecordSource, Store } from 'relay-runtime'
-import {
-    graphql,
-    preloadQuery,
-    usePreloadedQuery,
-    RelayEnvironmentProvider,
-} from 'react-relay/hooks'
+import { createRefetchContainer, graphql } from 'react-relay'
 import { Card, Content, Button } from '@workshop/ui'
-import { AppQuery } from './__generated__/AppQuery.graphql.js'
 
-function fetchQuery(operation, variables) {
-    return fetch('http://localhost:7500/graphql', {
-        method: 'POST',
-        credentials: 'same-origin', // 启用 cookie
-        headers: {
-            'Content-Type': 'application/json',
-        }, // Add authentication and other headers here
-        body: JSON.stringify({
-            query: operation.text, // GraphQL text from input
-            variables,
-        }),
-    }).then(response => {
-        return response.json()
-    })
-}
-
-const environment = new Environment({
-    network: Network.create(fetchQuery),
-    store: new Store(new RecordSource()),
-})
-
-const query = graphql`
-    query AppQuery {
-        posts {
-            edges {
-                node {
-                    id
-                    content
-                }
-            }
-        }
-    }
-`
-
-const result = preloadQuery(
-    environment,
-    query,
-    { first: '5' },
-    { fetchPolicy: 'store-or-network' }
-)
-
-const App = () => {
+const App = ({ feed }) => {
     const error = false
-    const data = usePreloadedQuery<AppQuery>(query, result)
 
     if (!data) {
         return (
@@ -69,7 +20,7 @@ const App = () => {
             <Flex flexDirection="column">
                 <Text>Posts</Text>
                 <Flex flexDirection="column">
-                    {data.posts.edges.map(({ node }) => (
+                    {feed.posts.edges.map(({ node }) => (
                         <Card
                             key={node.id}
                             mt="10px"
@@ -88,12 +39,29 @@ const App = () => {
     )
 }
 
-/* const App = React.lazy(() => import('./App')); // Lazy-loaded */
-
-export default () => (
-    <RelayEnvironmentProvider environment={environment}>
-        <Suspense fallback={<div>Loading</div>}>
-            <App />
-        </Suspense>
-    </RelayEnvironmentProvider>
-)
+export default createRefetchContainer(
+  App,
+  {
+    feed: graphql`
+      fragment App_feed on Query
+      @argumentDefinitions(
+        count: {type: "Int", defaultValue: 10}
+      ) {
+        posts(first: $count) {
+          edges {
+            node {
+              id
+              content
+            }
+          }
+        }
+      }
+    `
+  },
+  graphql`
+    # Notice that we re-use our fragment and the shape of this query matches our fragment spec.
+    query AppRefetchQuery($count: Int) {
+        ...App_feed @arguments(count: $count)
+    }
+  `,
+);
--- workshop/01-fetchGraphql/src/index.tsx
+++ workshop/01-fetchGraphql/src/index.tsx
@@ -1,7 +1,48 @@
 import 'isomorphic-fetch';
 import React from 'react';
+import { ReactRelayContext, graphql, QueryRenderer } from 'react-relay'
+import { Environment, Network, RecordSource, Store } from 'relay-runtime'
 import { createRoot } from 'react-dom';
 
 import App from './App';
 
-createRoot(document.getElementById('root')).render(<App />);
+
+function fetchQuery(operation, variables) {
+    return fetch('http://localhost:7500/graphql', {
+        method: 'POST',
+        credentials: 'same-origin', // 启用 cookie
+        headers: {
+            'Content-Type': 'application/json',
+        }, // Add authentication and other headers here
+        body: JSON.stringify({
+            query: operation.text, // GraphQL text from input
+            variables,
+        }),
+    }).then(response => {
+        return response.json()
+    })
+}
+
+const environment = new Environment({
+    network: Network.create(fetchQuery),
+    store: new Store(new RecordSource()),
+})
+
+
+createRoot(document.getElementById('root')).render(
+    <ReactRelayContext.Provider environment={environment}>
+    <QueryRenderer
+        environment={environment}
+        query={graphql`
+            query srcQuery {
+                ...App_feed
+            }
+        `}
+        variables={{
+            count: 10
+        }}
+        render={App}
+    />
+
+    </ReactRelayContext.Provider>
+);
